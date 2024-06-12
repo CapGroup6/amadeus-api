e0eaf02d4e206d78acc1f54cb3ad6832dd9b9b61
Xinling <lixl.xx@foxmail.com>
2024-06-11 17:12:13 -0700

    successfully build the Amadeus api

---
+++ amadeus-api/src/test/java/com/example/amadeusapi/AmadeusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amadeusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
