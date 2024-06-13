7fd7d01e9bcc80cd2de435a48aa4cc1cb3adde92
Liang Yan <98277677+lyim@users.noreply.github.com>
2024-06-13 00:32:18 -0700

    Add files via upload

--- amadeus-api/src/test/java/com/example/amadeusapi/AmadeusApiApplicationTests.java
+++ amadeus-api/src/test/java/com/example/amadeusapi/AmadeusApiApplicationTests.java
@@ -1,6 +1,13 @@
 package com.example.amadeusapi;
 
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.referenceData.Locations;
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.Location;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +17,32 @@ class AmadeusApiApplicationTests {
 	void contextLoads() {
 	}
 
+	@Value("${amadeus.apiKey}")
+	private String apiKey;
+
+	@Value("${amadeus.apiSecret}")
+	private String apiSecret;
+
+	@Test
+	void apiConnectionTest() {
+		System.out.println(apiKey);
+		System.out.println(apiSecret);
+
+		Amadeus amadeus = Amadeus
+				.builder(apiKey, apiSecret)
+				.build();
+
+        Location[] locations = null;
+        try {
+            locations = amadeus.referenceData.locations.get(Params
+                    .with("keyword", "LON")
+                    .and("subType", Locations.ANY));
+        } catch (ResponseException e) {
+            throw new RuntimeException(e);
+        }
+
+        int locationNum = locations.length;
+		System.out.println(locationNum);
+        for (Location location : locations) System.out.println(location.toString());
+	}
 }
